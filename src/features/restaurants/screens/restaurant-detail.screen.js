52b53e5e6749d4ff82356c94fc5586dc6002ac70
ltobar <luisadrianta1@gmail.com>
2021-03-16 12:23:09 -0500

    ADD: restauran menu component with an accordion list and add it to restaurant detail screen

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 import { RestaurantsInfoCard } from "../components/restaurant-info-card.component";
+import { RestaurantMenu } from "../../../features/restaurants/components/restaurant-menu-list";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 
 export const RestaurantDetailScreen = ({ route }) => {
   const { restaurant } = route.params;
 
-  return <RestaurantsInfoCard restaurant={restaurant} />;
+  return (
+    <>
+      <RestaurantsInfoCard restaurant={restaurant} />
+      <RestaurantMenu />
+    </>
+  );
 };
