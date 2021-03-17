e590dab66c363cdb80df0a4c213aa06d05c7ff82
ltobar <luisadrianta1@gmail.com>
2021-03-17 12:53:13 -0500

    ADD: async storage library for local storage of favourites

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,9 +1,29 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+
+  // Local storage favourites
+  const saveFavourites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@favourites", jsonValue);
+    } catch (e) {
+      console.log("storage error ", e);
+    }
+  };
+
+  const loadFavourites = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("@favourites");
+      return jsonValue !== null ? setFavourites(JSON.parse(jsonValue)) : null;
+    } catch (e) {
+      console.log("retrieve storage error ", e);
+    }
+  };
 
   const add = (restaurant) => {
     setFavourites([...favourites, restaurant]);
@@ -15,6 +35,14 @@ export const FavouritesContextProvider = ({ children }) => {
     );
     setFavourites(newFavorites);
   };
+
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
 
   return (
     <FavouritesContext.Provider
