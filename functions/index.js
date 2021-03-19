13e57600f4f067716380c142c39b9ecb7ec1e2e2
ltobar <luisadrianta1@gmail.com>
2021-03-19 17:50:36 -0500

    ADD: google function integration, now mocks are fetch from functions deployed in google functions

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+const functions = require("firebase-functions");
+
+const { geocodeRequest } = require("./geocode/index");
+const { placesRequest } = require("./places/index");
+
+exports.geocode = functions.https.onRequest((request, response) => {
+  geocodeRequest(request, response);
+});
+
+exports.placesNearby = functions.https.onRequest((request, response) => {
+  placesRequest(request, response);
+});
