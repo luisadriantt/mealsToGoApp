a0c2c273d1ef7bcb32c11b914f2e56e1821adf9d
ltobar <luisadrianta1@gmail.com>
2021-03-19 17:53:34 -0500

    FIX: fix git ignore

--- src/utils/env.js
+++
@@ -1,9 +0,0 @@
-import { Platform } from "react-native";
-
-const liveHost = "https://us-central1-mealstogo-b2612.cloudfunctions.net";
-const localHost = "http://localhost:5001/mealstogo-cf00d/us-central1";
-
-export const isAndroid = Platform.OS === "android";
-export const isDevelopment = process.env.NODE_ENV === "development";
-export const isMock = false;
-export const host = !isDevelopment || isAndroid ? liveHost : localHost;
